ea8a755415c3544365762a695a5e2b627a497a64
saitejat1907 <saitejat1907@gmail.com>
2024-07-31 00:21:43 -0500

    error resolve

--- vpn.tf
+++ vpn.tf
@@ -1,17 +1,21 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources-jenkins"
+  name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network-jenkins"
+  name                = "example-network"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_managed_disk" "example" {
-  name                 = "acctestmd-jenkins"
+  name                 = "acctestmd"
   location             = azurerm_resource_group.example.location
   resource_group_name  = azurerm_resource_group.example.name
   storage_account_type = "Standard_LRS"
