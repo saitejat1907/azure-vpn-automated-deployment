41e82726ca12fec25c5dea2380e7c065001ef79e
saitejat1907 <saitejat1907@gmail.com>
2024-07-31 00:18:59 -0500

    test

--- vpn.tf
+++ vpn.tf
@@ -1,17 +1,17 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "example-resources-jenkins"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "example-network-jenkins"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_managed_disk" "example" {
-  name                 = "acctestmd"
+  name                 = "acctestmd-jenkins"
   location             = azurerm_resource_group.example.location
   resource_group_name  = azurerm_resource_group.example.name
   storage_account_type = "Standard_LRS"
