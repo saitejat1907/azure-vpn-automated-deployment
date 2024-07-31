7b63c7050c7959770df5d2fda0eee6dcb3abd8fa
saitejat1907 <saitejat1907@gmail.com>
2024-07-30 19:40:50 -0500

    had disk and resource group

---
+++ vpn.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_managed_disk" "example" {
+  name                 = "acctestmd"
+  location             = azurerm_resource_group.example.location
+  resource_group_name  = azurerm_resource_group.example.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
+
+  tags = {
+    environment = "staging"
+  }
