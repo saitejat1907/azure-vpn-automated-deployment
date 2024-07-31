b2aee07751a4272d7f77a7d5521c9fee26284b18
saitejat1907 <saitejat1907@gmail.com>
2024-07-31 13:01:10 -0500

    updated parameters

--- vpn.tf
+++ vpn.tf
@@ -3,10 +3,10 @@ provider "azurerm" {
 
   features {}
 
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
+  # subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "example" {
